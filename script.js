0e73ba4bb4de13dcc48cdd5c25973a32c52020af
David _ <phiaphia97@yahoo.com>
2025-08-28 20:26:05 -0700

    Implement script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// select all keys
+const keys = document.querySelectorAll('.key');
+
+// Add an event listener to all keys
+keys.forEach((key) => {
+    key.addEventListener('click', () => playNote(key));
+});
+
+// Handlers
+function playNote(key) {
+    const noteAudio = document.getElementById(key.dataset.note);
+    noteAudio.play();
+}
