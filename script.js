c1269a41a80fd7ef4981b9c2831ee5acf8941638
David _ <phiaphia97@yahoo.com>
2025-08-28 20:52:21 -0700

    Implement script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+
 // select all keys
 const keys = document.querySelectorAll('.key');
 
@@ -9,5 +12,41 @@ keys.forEach((key) => {
 // Handlers
 function playNote(key) {
     const noteAudio = document.getElementById(key.dataset.note);
+
+    // reset the note's current time to 0
+    noteAudio.currentTime = 0;
+
+    // play the note
     noteAudio.play();
+
+    // add a class active for styling purposes
+    key.classList.add('active');
+
+    noteAudio.addEventListener('ended', () => {
+        key.classList.remove('active');
+    });
 }
+
+
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
+
+document.addEventListener('keydown', (e) => {
+  // Get the key that triggered the event
+  const key = e.key;
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+  const blackKeyIndex = BLACK_KEYS.indexOf(key);
+
+   // If the key is being held down, don't play the note again
+  if (e.repeat) {
+    return;
+  }
+
+  if (whiteKeyIndex > -1) {
+    playNote(whiteKeys[whiteKeyIndex]);
+  }
+
+  if (blackKeyIndex > -1) {
+    playNote(blackKeys[blackKeyIndex]);
+  }
+});
